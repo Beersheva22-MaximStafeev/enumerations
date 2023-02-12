81c9e76987823d7741a0e16818d2f8dd70239590
maxastaf <max.a.staf@gmail.com>
2023-02-12 17:27:31 +0200

    Initial commit

---
+++ enumerations/src/telran/time/DayOfWeek.java
@@ -0,0 +1,32 @@
+package telran.time;
+
+public enum DayOfWeek {
+	SUN, MON, TUE, WED, THU, FRI, SHABBAT;
+	public DayOfWeek plusDays(int days) {
+		DayOfWeek res = null;
+		if (days < 0) {
+			res = minusDays(-days);
+		} else {
+			int seqNumCurrent = ordinal();
+			int seqNumRes = (seqNumCurrent + days) % 7;
+			res = values()[seqNumRes];
+		}
+		return res;
+	}
+
+	public DayOfWeek minusDays(int days) {
+		DayOfWeek res = null;
+		if (days < 0) {
+			res = plusDays(-days);
+		} else {
+			int seqNumCurrent = ordinal();
+			int seqNumRes = (seqNumCurrent - days) % 7;
+//			seqNumRes = (seqNumRes + 7) % 7;
+			if (seqNumRes < 0) {
+				seqNumRes += 7;
+			}
+			res = values()[seqNumRes];
+		}
+		return res;
+	}
+}
